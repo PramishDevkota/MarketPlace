996ad0541d6675d49bcf01e8c51f0481e32c3bff
Pramish <pramishpramish1@gmail.com>
2026-08-30 00:11:43 +0545

    Configured for Vercel

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-vx!0!*p&z$fawjgyd@r-v
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
+ALLOWED_HOSTS = (
     'localhost',
     '127.0.0.1',
     '.vercel.app',
