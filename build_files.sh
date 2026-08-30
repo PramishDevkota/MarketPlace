af442f6bab57f8a3923582a138767608aacc0099
Pramish <pramishpramish1@gmail.com>
2026-08-30 12:21:44 +0545

    Fix Vercel pip install PEP 668 error using --break-system-packages

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+# Install dependencies overriding uv/PEP 668 restriction
+python3 -m pip install --break-system-packages -r requirements.txt
+
+# Run collectstatic
+python3 manage.py collectstatic --noinput --clear
