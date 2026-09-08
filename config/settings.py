7b79b06251685b787ac294d7482e892d442b6e75
Pramish <pramishpramish1@gmail.com>
2026-09-08 11:31:13 +0545

    updated about section

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@islingtonmarketplace.com')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='pramishpramish1@gmail.com')
 
 # WhatsApp Cloud API credentials (optional; used when configured).
 WHATSAPP_TOKEN = config('WHATSAPP_TOKEN', default='')
